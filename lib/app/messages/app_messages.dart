0a21f123d8154278fd8dcd48a69233e66b189834
mdtavangar <md.tavangar79@gmail.com>
2024-03-22 15:07:48 +0330

    fix splash tips message

--- lib/app/messages/app_messages.dart
+++ lib/app/messages/app_messages.dart
@@ -1,7 +1,7 @@
 class AppMessage {
   static const String home = 'Home';
   static const String splashLoading = 'Loading Data please wait...';
-  static const String splashTip = 'Please be Ensure your VPN is On';
+  static const String splashTip = 'Please wait ...';
 
   //Details texts
   static const String dFavo = 'Favo :';
