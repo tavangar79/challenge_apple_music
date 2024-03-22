89ca251f97208ae5f70a38a025840aff843f370c
mdtavangar <md.tavangar79@gmail.com>
2024-03-22 15:11:59 +0330

    add home page route

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,7 +1,11 @@
 import 'package:apple_music/app/routes/app_routes.dart';
+import 'package:apple_music/feature/home/view/home_page.dart';
 import 'package:apple_music/feature/splash/view/splash_page.dart';
 import 'package:get/get.dart';
 
 class AppPages {
-  final pages = [GetPage(name: Routes.splash, page: SplashPage.new)];
+  final pages = [
+    GetPage(name: Routes.splash, page: SplashPage.new),
+    GetPage(name: Routes.home, page: HomePage.new),
+  ];
 }
