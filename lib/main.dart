c66b8796abe8eda86dadda20c75e5209a5cd2d90
mdtavangar <md.tavangar79@gmail.com>
2024-03-22 15:06:50 +0330

    fix main (add screenUtilInit)

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:apple_music/app/binding/main_binding.dart';
 import 'package:apple_music/app/resources/app_theme.dart';
 import 'package:apple_music/app/routes/app_pages.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
 void main() {
@@ -13,16 +14,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      title: 'Apple Music',
-      debugShowCheckedModeBanner: true,
-      initialRoute: "/splash",
-      theme: AppThemes().lightTheme,
-      defaultTransition: Transition.fade,
-      initialBinding: MainBinding(),
-      supportedLocales: const [Locale('en')],
-      fallbackLocale: const Locale('en'),
-      getPages: AppPages().pages,
+    return ScreenUtilInit(
+      builder: (final context, widget) => GetMaterialApp(
+        title: 'Apple Music',
+        debugShowCheckedModeBanner: true,
+        initialRoute: "/splash",
+        theme: AppThemes().lightTheme,
+        defaultTransition: Transition.fade,
+        initialBinding: MainBinding(),
+        supportedLocales: const [Locale('en')],
+        fallbackLocale: const Locale('en'),
+        getPages: AppPages().pages,
+      ),
     );
   }
 }
